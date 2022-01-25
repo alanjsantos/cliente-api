a60fa398255287aa907bc9fbf4857b96fc010dd6
Alan Santos <alanjosedossantos011@gmail.com>
2022-01-25 12:07:40 -0300

    CRUD conlcuido com docker

--- src/main/java/com/apicliente/repository/ClienteReposieoty.java
+++ src/main/java/com/apicliente/repository/ClienteReposieoty.java
@@ -4,6 +4,11 @@ import com.apicliente.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClienteReposieoty extends JpaRepository<Cliente, Long> {
+
+
+    Optional<Cliente> findByEmail(String email);
 }
--- src/main/java/com/apicliente/service/ClienteService.java
+++ src/main/java/com/apicliente/service/ClienteService.java
@@ -2,8 +2,12 @@ package com.apicliente.service;
 
 import com.apicliente.model.Cliente;
 import com.apicliente.repository.ClienteReposieoty;
+import com.apicliente.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClienteService {
@@ -13,6 +17,33 @@ public class ClienteService {
 
     public Cliente save(Cliente cliente) {
         return reposieoty.save(cliente);
+    }
+
+    public List<Cliente> findAll() {
+        return reposieoty.findAll();
+    }
+
+    public Cliente findById(Long id) {
+        Optional<Cliente> obj = reposieoty.findById(id);
+
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Id " + id + " não encontrado na base de dados"));
+    }
+
+    public Cliente update (Cliente cliente) {
+        findById(cliente.getId());
+
+        return reposieoty.save(cliente);
+    }
+
+    public void delete (Long id) {
+        findById(id);
+        reposieoty.deleteById(id);
+    }
+
+    public Cliente findByEmail(String email) {
+        Optional<Cliente> obj = reposieoty.findByEmail(email);
+
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Email " + email  + " não encontrado na base de dados "));
     }
 
 }
